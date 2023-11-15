d41eb6c1a62532fa10df8a55b25b2ac18ff8ca48
B00147423 <b00147423@mytudublin.ie>
2023-11-15 12:44:38 +0000

    fixxed the registration page, now it sends the data to the collection. it can cconnect to the database now.
    fixed the file structure and made the registration page. also got the registration page working where now it can connect to the database and Post the detail user entered in the database.

--- major-group-project/src/app/page.js
+++ major-group-project/src/app/page.js
@@ -1,51 +1,52 @@
+// Home.js
 "use client";
-//Login form for draft/project website
 import React, { useEffect } from "react";
 import Helmet from 'react-helmet';
 import VanillaTilt from 'vanilla-tilt';
 import "./css/AboutUs.css";
 import RedirectButton from './Components/RedirectButton';
-
 export default function Home() {
 
-  //UseEffect hook for card animations
+  
+  // useEffect hook for card animations
   useEffect(() => {
-    // Initialize VanillaTilt within the useEffect hook
     VanillaTilt.init(document.querySelectorAll(".card"), {
-        max: 25,
-        speed: 400,
+      max: 25,
+      speed: 400,
     });
-}, []); // Empty dependency array to ensure the effect runs only once
-
+  }, []); // Empty dependency array to ensure the effect runs only once
+ 
   return (
-    <><div class="container">
-            <div class="card">
-                <div class="card-content">
-                    <h2>01</h2>
-                    <h3>Sign Up</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem quas sapiente architecto, atque
-                        accusamus itaque minus neque accusantium veritatis cupiditate.</p>
-                        <div class="button">
-                        <RedirectButton destination="/SignUp" text="Sign Up" />
-                        </div>
-                </div>
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="card-content">
+            <h2>01</h2>
+            <h3>Sign Up</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem quas sapiente architecto, atque
+              accusamus itaque minus neque accusantium veritatis cupiditate.</p>
+            <div className="button">
+              <RedirectButton destination="./signup" text="Sign Up" />
             </div>
-            <div class="card">
-                <div class="card-content">
-                    <h2>02</h2>
-                    <h3>Login</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora officia debitis quis velit sunt
-                        error sapiente odit aliquid distinctio doloremque repudiandae praesentium, earum minus omnis esse
-                        quo cum iusto. Repudiandae!</p>
-                        <div class="button">
-                        <RedirectButton destination="/pages/Login" text="Login" />
-                        </div>
-                </div>
-            </div>
+          </div>
         </div>
-        {/* Use of the Helmet package for react to allow for the addition of scripts */}
-        <Helmet>
-            <script src="https://cdnjs.cloudfare.com/ajax/libs/vanilla-tilt/1.7.2/vanilla-tilt.min.js"></script>
-        </Helmet></>
-  )
+        <div className="card">
+          <div className="card-content">
+            <h2>02</h2>
+            <h3>Login</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora officia debitis quis velit sunt
+              error sapiente odit aliquid distinctio doloremque repudiandae praesentium, earum minus omnis esse
+              quo cum iusto. Repudiandae!</p>
+            <div className="button">
+            <RedirectButton destination="./login" text="Login" />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Use of the Helmet package for react to allow for the addition of scripts */}
+      <Helmet>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.2/vanilla-tilt.min.js"></script>
+      </Helmet>
+    </>
+  );
 }
---
+++ major-group-project/src/app/signup/page.js
@@ -0,0 +1,89 @@
+"use client";
+import React, { useState } from 'react';
+
+export default function Page() {
+
+  async function runDBCallAsync(url, formData) {
+    try {
+      const res = await fetch(url, {
+        method: 'POST', // Use POST method
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      // Check if the HTTP status code is OK (200-299)
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+  
+      // Try to parse the response as JSON
+      const data = await res.json();
+  
+      if (data.data === "valid") {
+        console.log("Registration Successful!");
+      } else {
+        console.log("Error: Registration unsuccessful");
+      }
+    } catch (error) {
+      // If an error occurs, log it to the console
+      console.error("Error during fetch: ", error);
+    }
+  }
+
+  const handleSubmit = (event) => {
+    console.log("handling submit");
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    let username = data.get('username');
+    let email = data.get('email');
+    let pass = data.get('pass');
+    let repeatPass = data.get('repeatPass');
+    let address = data.get('address');
+    let dob = data.get('dob');
+
+    runDBCallAsync(`http://localhost:3000/api/register?email=${email}&pass=${pass}&repeatPass=${repeatPass}&username=${username}&address=${address}&dob=${dob}`);
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" id="username" />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="text" name="email" id="email" />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="pass" id="pass" />
+        </label>
+        <br />
+        <label>
+          Repeat Password:
+          <input type="password" name="repeatPass" id="repeatPass" />
+        </label>
+        <br />
+        <label>
+          Date of Birth:
+          <input type="text" name="dob" id="dob" />
+        </label>
+        <br />
+        <label>
+          Address:
+          <input type="text" name="address" id="address" />
+        </label>
+        <br />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
