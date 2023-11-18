3a56bffa3d8ee388b84115668528e29d00ae3eb0
B00147423 <b00147423@mytudublin.ie>
2023-11-18 11:10:01 +0000

    update files
    pushed files that  didnt get updated

---
+++ major-group-project/src/app/Components/RedirectButton.js
@@ -0,0 +1,25 @@
+// RedirectButton.js
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+//RedirectButton with "destination" and "text" props passed through
+const RedirectButton = ({ destination, text }) => {
+  //Use Next Router
+  const router = useRouter();
+
+  //Send user to destination page when clicked
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push(destination);
+  };
+
+  //Component Content
+  return (
+    <a href={destination} onClick={handleClick}>
+    {text}
+    </a>
+  );
+};
+
+//Export component so it can be used later
+export default RedirectButton;
