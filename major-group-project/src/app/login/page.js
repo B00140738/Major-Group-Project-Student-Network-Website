075af512997f208263d993082bac0586f776c2aa
B00147423 <b00147423@mytudublin.ie>
2023-11-22 15:31:01 +0000

    fix changes to login page so that when user enters username and password and the yare registered it retrieves it from api and logs them in

--- major-group-project/src/app/login/page.js
+++ major-group-project/src/app/login/page.js
@@ -1,67 +1,85 @@
 "use client"
-import React, { useState, useEffect } from 'react';
-import '../css/loginform.css'; // Make sure this path is correct
+import React, { useState } from 'react';
+import '../css/loginform.css';
 
-const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [popupStyle, setPopupStyle] = useState("hide");
-  const [isClient, setIsClient] = useState(false)
-  const showPopup = () => {
-    // Check if either the username or password is empty
-    if (!username || !password) {
-      // Specify what popup to show
-      setPopupStyle("login-popup-show");
 
-      // Popup duration
-      setTimeout(() => {
-        setPopupStyle("hide");
-      }, 3000);
-    } else {
-      // Here you would put your login logic
-      console.log('Attempt login with:', username, password);
+export default function Login() {
+  const [popupStyle, setPopupStyle] = useState('hide');
+  const [popupMessage, setPopupMessage] = useState('');
+
+  async function runDBCallAsync(url) {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      if (!res.ok) {
+        throw new error(`HTTP error! status: ${res.status}`);E
+      }
+
+     
+
+      if (data.data == "true") {
+        console.log("Login Successful!");
+       
+        // Redirect or perform further actions upon successful login
+      } else {
+        throw new Error('Invalid username or password');
+        
+      }
+    } catch (error) {
+      setPopupMessage(error.message);
+     
+      setPopupStyle('login-popup-show');
+      setTimeout(() => setPopupStyle('hide'), 3000);
     }
-  };
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
- 
-  return ( 
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    let email = data.get('email');
+    let pass = data.get('pass');
+
+      runDBCallAsync(`http://localhost:3000/api/login?&email=${email}&pass=${pass}`);
     
+  };
+  
+  return (
     <div className="loginContainer">
       <div className="loginBox">
-        <h1> Sign in</h1>
+        <h1>Sign in</h1>
         <button className="appleLogin">Sign in with Apple</button>
         <button className="googleLogin">Sign in with Google</button>
         <div className="divider">or</div>
-        <input
-          type="text"
-          placeholder="Phone, email, or username"
-          className="inputField"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="inputField"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="button" className="nextButton" onClick={showPopup}>Next</button>
-
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Phone, email, or username"
+            className="inputField"
+          
+            name="username"
+            aria-label="Username"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="inputField"
+            name="pass"
+           
+            aria-label="Password"
+          />
+          <button type="submit" className="nextButton">Next</button>
+        </form>
         <a href="#" className="forgotPassword">Forgot password?</a>
         <div className="signUp">
           Don't have an account? <a href="#" className="signUpLink">Sign up</a>
         </div>
         <div className={popupStyle}>
           <h3>Login Failed!</h3>
-          <p>Username or Password incorrect</p>
+          <p>{popupMessage}</p>
         </div>
       </div>
     </div>
   );
-
 };
 
-export default Login;
