2bb06702445ef46ac87863ba29447ba702433af4
B00147423 <b00147423@mytudublin.ie>
2023-12-02 11:41:22 +0000

    added cookeis to main branch

--- major-group-project/src/app/dashboard/page.js
+++ major-group-project/src/app/dashboard/page.js
@@ -1,30 +1,43 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/dashboard.css';
 
 export default function Dashboard() {
+    const [username, setUsername] = useState('');
+
+    useEffect(() => {
+      // Retrieve username from the cookie
+      const getUsernameFromCookies = () => {
+        const allCookies = document.cookie.split('; ');
+        const usernameCookie = allCookies.find(cookie => cookie.startsWith('username='));
+        return usernameCookie ? decodeURIComponent(usernameCookie.split('=')[1]) : '';
+      };
+
+      setUsername(getUsernameFromCookies());
+    }, []);
+
     return (
-        <div class="main-container">
-        <div class="dashboard-container">
-            <header>
-                <div class="search-bar">
-                    <input type="text" placeholder="Search"/>
-                    <button class="search-button">Search</button>
-                </div>
-                <button class="profile-button">Profile</button>
-            </header>
-            <section class="main-content">
-                <div class="top-boxes">
-                    <div class="forum-box">
-                        <h2>Forums</h2>
-                        <button class="view-posts-button">View Posts</button>
+        <div className="main-container">
+            <div className="dashboard-container">
+                <header>
+                    <div className="search-bar">
+                        <input type="text" placeholder="Search"/>
+                        <button className="search-button">Search</button>
                     </div>
-                </div>
-                <div class="side-bar">
-                    <h2>General Forums</h2>
-                </div>
-            </section>
+                    <button className="profile-button">{username}</button>
+                </header>
+                <section className="main-content">
+                    <div className="top-boxes">
+                        <div className="forum-box">
+                            <h2>Forums</h2>
+                            <button className="view-posts-button">View Posts</button>
+                        </div>
+                    </div>
+                    <div className="side-bar">
+                        <h2>General Forums</h2>
+                    </div>
+                </section>
+            </div>
         </div>
-    </div>
-    )
+    );
 }
