c5f0b2ad2809e31ffc823e532d34a3274d6b9734
B00147423 <b00147423@mytudublin.ie>
2023-12-06 16:24:58 +0000

    added logout to dashboard so now users can log out if they wish

--- major-group-project/src/app/dashboard/page.js
+++ major-group-project/src/app/dashboard/page.js
@@ -1,20 +1,43 @@
 "use client"
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import '../css/dashboard.css';
 
 export default function Dashboard() {
     const [username, setUsername] = useState('');
-
+    const [showDropdown, setShowDropdown] = useState(false);
+    const router = useRouter(); // Get the Next.js router
     useEffect(() => {
-      // Retrieve username from the cookie
-      const getUsernameFromCookies = () => {
+        
+        // Retrieve username from the cookie
+        const getUsernameFromCookies = () => {
         const allCookies = document.cookie.split('; ');
         const usernameCookie = allCookies.find(cookie => cookie.startsWith('username='));
         return usernameCookie ? decodeURIComponent(usernameCookie.split('=')[1]) : '';
       };
+      const username = getUsernameFromCookies();
+      setUsername(username);
+      // If the username is not set, redirect to the login page
+        if (!username) {
+            router.replace('/'); // replace the '/login' with your login route
+  }
+    }, [router]);
 
-      setUsername(getUsernameFromCookies());
-    }, []);
+
+
+       // Toggle dropdown
+       const toggleDropdown = () => setShowDropdown(!showDropdown);
+
+      // Logout function
+      const handleLogout = () => {
+        // Clear the username cookie or other session storage
+        document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        // Redirect to the home page if the router is available
+    if (router) {
+       
+        router.push('/');
+    }
+      };
 
     return (
         <div className="main-container">
@@ -24,7 +47,18 @@ export default function Dashboard() {
                         <input type="text" placeholder="Search"/>
                         <button className="search-button">Search</button>
                     </div>
-                    <button className="profile-button">{username}</button>
+                    <div className="profile-section">
+                        <button onClick={toggleDropdown} className="profile-button">
+                            {username} {showDropdown ? '▲' : '▼'}
+                        </button>
+                        {showDropdown && (
+                            <div className="profile-dropdown">
+                            <div className="dropdown-item">Settings</div>
+                            <div onClick={handleLogout} className="dropdown-item">Log out @{username}</div>
+                            </div>
+                        )}
+                    </div>
+
                 </header>
                 <section className="main-content">
                     <div className="top-boxes">
