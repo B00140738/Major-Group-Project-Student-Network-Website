7d78a6e633af65136d6d9342ac2d8172551cf17b
B00147423 <b00147423@mytudublin.ie>
2023-12-06 16:29:00 +0000

    error handling users now has to repeat correct password, users cant sign up with emtpy fields, positioend the error messages and fixed date of birth now it has dropdowns

--- major-group-project/src/app/signup/page.js
+++ major-group-project/src/app/signup/page.js
@@ -1,89 +1,139 @@
 "use client";
+// SignUpModal.js
 import React, { useState } from 'react';
+import '../css/signupform.css'; // Adjust the path to your CSS file
 
-export default function Page() {
+export default function SignUpModal({ toggleModal }) {
+const range = (start, end) => Array.from({ length: end - start + 1 }, (_, i) => start + i);
+const [errorMessage, setErrorMessage] = useState('');
+const [passwordError, setPasswordError] = useState(''); // Separate state for password error
 
-  async function runDBCallAsync(url, formData) {
-    try {
-      const res = await fetch(url, {
-        method: 'POST', // Use POST method
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-  
-      // Check if the HTTP status code is OK (200-299)
-      if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}`);
+    async function runDBCallAsync(url, formData) {
+        try {
+          const res = await fetch(url, {
+            method: 'POST', // Use POST method
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+      
+          // Check if the HTTP status code is OK (200-299)
+          if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+          }
+      
+          // Try to parse the response as JSON
+          const data = await res.json();
+      
+          if (data.data === "valid") {
+            console.log("Registration Successful!");
+          } else {
+            console.log("Error: Registration unsuccessful");
+            setErrorMessage("Registration unsuccessful"); // Set the error message
+          }
+        } catch (error) {
+          // If an error occurs, log it to the console
+          console.error("Error during fetch: ", error);
+          setErrorMessage(error.message);
+        }
       }
-  
-      // Try to parse the response as JSON
-      const data = await res.json();
-  
-      if (data.data === "valid") {
-        console.log("Registration Successful!");
-      } else {
-        console.log("Error: Registration unsuccessful");
-      }
-    } catch (error) {
-      // If an error occurs, log it to the console
-      console.error("Error during fetch: ", error);
-    }
-  }
+    
+      const handleSubmit = (event) => {
+        event.preventDefault();
+      setErrorMessage(''); // Clear any existing error messages
+      setPasswordError(''); 
+        const data = new FormData(event.currentTarget);
+    
+        let username = data.get('username');
+        let email = data.get('email');
+        let pass = data.get('pass');
+        let address = data.get('address');
+        let repeatPass = data.get('repeatPass');
+        const month = data.get('dobMonth');
+        const day = data.get('dobDay');
+        const year = data.get('dobYear');
+        const dob = `${year.padStart(2, '0')}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
 
-  const handleSubmit = (event) => {
-    console.log("handling submit");
-    event.preventDefault();
+         // Pseudo-code for checking if the user already exists
+ 
+        
+        if (pass !== repeatPass) {
+          setPasswordError("Passwords do not match."); // Set the error message
+          return;
+        } else if(!username || !email || !pass || !address || !dob){
+          setErrorMessage("Please fill in the form"); // Set the error message
+        }
+   
+       
+          else{
+          runDBCallAsync(`http://localhost:3000/api/register?&username=${username}&email=${email}&pass=${pass}&address=${address}&dob=${dob}`);
 
-    const data = new FormData(event.currentTarget);
-
-    let username = data.get('username');
-    let email = data.get('email');
-    let pass = data.get('pass');
-    let repeatPass = data.get('repeatPass');
-    let address = data.get('address');
-    let dob = data.get('dob');
-
-    runDBCallAsync(`http://localhost:3000/api/register?&username=${username}&email=${email}&pass=${pass}&repeatPass=${repeatPass}&address=${address}&dob=${dob}`);
-  };
-
+        };
+      
+       
+      };
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" name="username" id="username" />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input type="text" name="email" id="email" />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" name="pass" id="pass" />
-        </label>
-        <br />
-        <label>
-          Repeat Password:
-          <input type="password" name="repeatPass" id="repeatPass" />
-        </label>
-        <br />
-        <label>
-          Date of Birth:
-          <input type="text" name="dob" id="dob" />
-        </label>
-        <br />
-        <label>
-          Address:
-          <input type="text" name="address" id="address" />
-        </label>
-        <br />
-        <button type="submit">Register</button>
-      </form>
+    <div className="modal-background">
+      <div className="modal-container">
+        <button onClick={toggleModal} className="close-modal">X</button>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input type="text" name="username" id="username" className="inputField"/>
+          </label>
+          <br />
+          <label>
+            Email:
+            <input type="text" name="email" id="email"  className="inputField"/>
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="password" name="pass" id="pass" className="inputField"/>
+          </label>
+          <br />
+          <label>
+            Repeat Password:
+            <input type="password" name="repeatPass" id="repeatPass" className="inputField"/>
+            {passwordError && <div className="error-message">{passwordError}</div>}
+          </label>
+          <br />
+          <label>
+            Date of Birth:
+            <div className="dob-select">
+              <select name="dobMonth" id="dobMonth" >
+                <option value="">Month</option>
+                {range(1, 12).map(month => (
+                  <option key={month} value={month}>{month}</option>
+                ))}
+              </select>
+              <select name="dobDay" id="dobDay">
+                <option value="">Day</option>
+                {range(1, 31).map(day => (
+                  <option key={day} value={day}>{day}</option>
+                ))}
+              </select>
+              <select name="dobYear" id="dobYear">
+                <option value="">Year</option>
+                {range(new Date().getFullYear() - 100, new Date().getFullYear()).map(year => (
+                  <option key={year} value={year}>{year}</option>
+                ))}
+              </select>
+            </div>
+          </label>
+          <br />
+          <label>
+            Address:
+            <input type="text" name="address" id="address" className="inputField"/>
+          </label>
+          <br />
+          <button type="submit">Register</button>
+        </form>
+      </div>
     </div>
   );
-}
+};
+
