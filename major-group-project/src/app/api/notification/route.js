d1c5550ae70fc3aba616210ac51ceebd5d09b707
B00147423 <b00147423@mytudublin.ie>
2024-04-05 16:14:24 +0100

    notifications api made by Beka b00147423, woirking, email etc

--- major-group-project/src/app/api/notification/route.js
+++ major-group-project/src/app/api/notification/route.js
@@ -1,22 +1,38 @@
-// Assuming this is in a file located at app/api/notifications/route.js (Next.js 13.x new folder structure)
-import { MongoClient, ObjectId } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import { NextResponse } from "next/server";
+import cookie from 'cookie'; // Ensure you have 'cookie' installed or use an equivalent method
 
-export async function GET(req) {
+export async function GET(request) {
     const url = 'mongodb://root:example@localhost:27017/';
     const client = new MongoClient(url);
     const dbName = 'forums';
-    const searchParams = new URL(req.url).searchParams;
-    const username = searchParams.get('username'); // Assuming username is unique
 
     try {
         await client.connect();
         const db = client.db(dbName);
-        const notification = await db.collection('notifications').findOne({ username });
 
-        return NextResponse.json({ count: notification ? notification.count : 0 });
+        // Parse the request cookies
+        const cookies = cookie.parse(request.headers.get('cookie') || '');
+        const currentUsername = cookies.username; // Assuming 'username' is the cookie name
+        console.log('Current username from cookie:', currentUsername); // This will log the username from the cookie
+
+        // Ensure that a username is available
+        if (!currentUsername) {
+            throw new Error('Username not found in cookies');
+        }
+
+        const notificationsCollection = db.collection('notifications');
+        // Find the document for the current user
+        const userDoc = await notificationsCollection.findOne({ username: currentUsername });
+
+        // If the userDoc exists, filter out the notifications created by the current user
+        // If not, return an empty array
+        const userNotifications = userDoc ? userDoc.notifications.filter(notification => notification.createdBy !== currentUsername) : [];
+
+        // Now you have the notifications specific to the current user
+        return NextResponse.json({ notifications: userNotifications }, { status: 200 });
     } catch (error) {
-        console.error('Error fetching notification count:', error);
+        console.error('Error fetching notifications:', error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     } finally {
         await client.close();
